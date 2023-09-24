155fe4761bb22ff3235f854bcb828ff4294eba1a
daniladolzhikov <d.dolzhikov17@gmail.com>
2023-09-24 12:38:51 +0200

    Copying elements from arr1 to arr2

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -1,2 +1,11 @@
 ﻿string[] arr1 = new string[4] { "2", "hello", "world", ":-)" };
 string[] arr2 = new string[arr1.Length];
+int j = 0;
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= 3)
+    {
+        arr2[j] = arr1[i];
+        j++;
+    }
+}
